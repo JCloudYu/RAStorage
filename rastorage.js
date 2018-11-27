05c1b1a9e34104a469615124e9e8b657de4c6970
JCloudYu <j.cloud.yu@gmail.com>
2018-11-27 11:56:38 +0800

    Initial commit

---
+++ rastorage.js
@@ -0,0 +1,33 @@
+/**
+ *	Author: JCloudYu
+ *	Create: 2018/11/27
+**/
+(()=>{
+	"use strict";
+	
+	class RAStorage {
+		constructor() {
+		
+		}
+		async put() {
+		
+		}
+		async overwrite() {
+		
+		}
+		async get() {
+		
+		}
+		async del() {
+		
+		}
+		
+		
+		
+		static initAtPath(path) {
+			return new RAStorage(path);
+		}
+	}
+	
+	module.exports = RAStorage;
+})();
